e64d1894ac0c496097fd08a88f299d2787ec08ad
AenSaevherne23 <kacper.cisek12@gmail.com>
2024-06-03 12:20:19 +0200

    polaczenie

--- index.php
+++ index.php
@@ -11,8 +11,41 @@
 <body>
     
 <?php
+$serverName = "localhost\\SQLEXPRESS";
+$database = "leclerc";
+$username = "sa";
+$password = "123456789";
 
+// Połączenie za pomocą SQLSRV
+$connectionInfo = array("Database" => $database, "UID" => $username, "PWD" => $password);
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
+if ($conn) {
+    echo "Połączenie z bazą danych powiodło się!";
+} else {
+    echo "Błąd połączenia: ";
+    die(print_r(sqlsrv_errors(), true));
+}
+?>
+<br />
+<h3>Testowe zapytanie</h3>
+
+<?php
+$zapytanie = "SELECT TOP (100) [tk_id], [tk_data], [tk_siec], [tk_plu], [tk_cena], [tk_zweryfikowane] FROM [leclerc].[dbo].[tw_konkurencja]";
+$wynik = $conn->query($zapytanie);
+
+if($wynik->num_rows > 0)
+{
+    while($wiersz = $wynik->fetch_assoc())
+    {
+        echo "<li>".$wiersz["tk_id"]." ".$wiersz["tk_data"]." ".$wiersz["tk_siec"]." ".$wiersz["tk_plu"]." ".$wiersz["tk_cena"]." ".$wiersz["tk_zweryfikowane"]."</li>";
+    }
+}
+else
+{
+    echo "BRAK";
+}
+$conn->close();
 ?>
 
 </body>
