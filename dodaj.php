7572e810a44ed632ae15242e8b568b005f36fa78
AenSaevherne23 <kacper.cisek12@gmail.com>
2024-06-06 14:34:44 +0200

    Dodawanie z dobrym filtrowaniem

--- dodaj.php
+++ dodaj.php
@@ -15,6 +15,11 @@
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         require_once("config.php");
 
+        // Funkcja zaokrąglająca cenę do drugiego miejsca po przecinku
+        function zaokraglij_do_drugiego_miejsca($cena) {
+            return round($cena, 1) - 0.01;
+        }
+
         // Zapytanie SQL dla unikalnych numerów EAN z tabeli, ograniczone do 500 rekordów
         $zapytanie_eany = "SELECT DISTINCT TOP(500) [tk_plu] FROM [leclerc].[dbo].[tw_konkurencja] GROUP BY [tk_plu] HAVING COUNT(*) > 1";
         $wynik_eany = sqlsrv_query($conn, $zapytanie_eany);
@@ -45,101 +50,124 @@
                 $cenyProduktow = array();
 
                 while ($wiersz = sqlsrv_fetch_array($wynik, SQLSRV_FETCH_ASSOC)) {
-                    $tk_cena = round($wiersz["tk_cena"], 1) - 0.01;
+                    $tk_cena = zaokraglij_do_drugiego_miejsca($wiersz["tk_cena"]);
                     $cenyProduktow[] = $tk_cena;
                 }
 
-                // Obliczanie mediany
-                sort($cenyProduktow);
-                $liczbaCenProduktow = count($cenyProduktow);
-                $medianaProduktow = $liczbaCenProduktow % 2 == 0 ? ($cenyProduktow[$liczbaCenProduktow / 2 - 1] + $cenyProduktow[$liczbaCenProduktow / 2]) / 2 : $cenyProduktow[($liczbaCenProduktow - 1) / 2];
-
-                // Obliczanie odchylenia standardowego
-                $sumaOdchylenProduktow = 0;
-                foreach ($cenyProduktow as $cena) {
-                    $sumaOdchylenProduktow += pow($cena - $medianaProduktow, 2);
+                if (empty($cenyProduktow)) {
+                    continue;
                 }
-                $odchylenieStandardoweProduktow = sqrt($sumaOdchylenProduktow / $liczbaCenProduktow);
-                
-                // Filtrowanie cen odstających
-                $cenyFiltrProduktow = array_filter($cenyProduktow, function($cena) use ($medianaProduktow, $odchylenieStandardoweProduktow) {
-                    return $cena <= $medianaProduktow + 2.3 * $odchylenieStandardoweProduktow;
-                });
-                
-                // Obliczanie średniej ceny
-                $sumaCenProduktow = array_sum($cenyFiltrProduktow);
-                $liczbaProduktowFiltr = count($cenyFiltrProduktow);
-                $sredniaCenaProduktow = $liczbaProduktowFiltr ? $sumaCenProduktow / $liczbaProduktowFiltr : 0;
-                
-                // Obliczanie mediany z przefiltrowanych cen
-                sort($cenyFiltrProduktow);
-                $medianaFiltrProduktow = $liczbaProduktowFiltr % 2 == 0 ? ($cenyFiltrProduktow[$liczbaProduktowFiltr / 2 - 1] + $cenyFiltrProduktow[$liczbaProduktowFiltr / 2]) / 2 : $cenyFiltrProduktow[($liczbaProduktowFiltr - 1) / 2];
-                
-                // Obliczanie dominanty z przefiltrowanych cen
-                $dominantaProduktow = array_count_values(array_map('strval', $cenyFiltrProduktow));
-                $maxOccurrencesProduktow = max($dominantaProduktow);
-                $pierwszaDominantaProduktow = array_search($maxOccurrencesProduktow, $dominantaProduktow);
 
-                foreach ($dominantaProduktow as $key => $value) {
-                    if ($value === $maxOccurrencesProduktow && $key > $pierwszaDominantaProduktow) {
-                        $pierwszaDominantaProduktow = $key;
+                $czyFiltruj = true;
+
+                while ($czyFiltruj) {
+                    // Sortowanie cen
+                    sort($cenyProduktow);
+
+                    // Obliczenie mediany
+                    $liczbaCen = count($cenyProduktow);
+                    if ($liczbaCen % 2 == 0) {
+                        $mediana = ($cenyProduktow[$liczbaCen / 2 - 1] + $cenyProduktow[$liczbaCen / 2]) / 2;
+                    } else {
+                        $mediana = $cenyProduktow[($liczbaCen - 1) / 2];
+                    }
+
+                    // Obliczenie odchylenia standardowego
+                    $sumaOdchylen = 0;
+                    foreach ($cenyProduktow as $cena) {
+                        $sumaOdchylen += pow($cena - $mediana, 2);
+                    }
+                    $odchylenieStandardowe = sqrt($sumaOdchylen / $liczbaCen);
+
+                    // Filtrowanie cen odstających
+                    $cenyFiltr = array_filter($cenyProduktow, function($cena) use ($mediana, $odchylenieStandardowe) {
+                        return $cena >= $mediana - 2.3 * $odchylenieStandardowe && $cena <= $mediana + 2.3 * $odchylenieStandardowe;
+                    });
+
+                    // Sprawdzenie, czy są jakieś ceny do odfiltrowania
+                    if (count($cenyFiltr) == count($cenyProduktow)) {
+                        $czyFiltruj = false;
+                    } else {
+                        $cenyProduktow = $cenyFiltr;
+                    }
+                }
+
+                // Obliczenie średniej ceny z przefiltrowanych cen
+                $sumaCen = array_sum($cenyProduktow);
+                $liczbaProduktowFiltr = count($cenyProduktow);
+                $sredniaCenaProduktow = $liczbaProduktowFiltr ? $sumaCen / $liczbaProduktowFiltr : 0;
+
+                // Obliczenie mediany z przefiltrowanych cen
+                sort($cenyProduktow);
+                if ($liczbaProduktowFiltr % 2 == 0) {
+                    $medianaFiltr = ($cenyProduktow[$liczbaProduktowFiltr / 2 - 1] + $cenyProduktow[$liczbaProduktowFiltr / 2]) / 2;
+                } else {
+                    $medianaFiltr = $cenyProduktow[($liczbaProduktowFiltr - 1) / 2];
+                }
+
+                // Obliczenie dominanty z przefiltrowanych cen
+                $dominanta = array_count_values(array_map('strval', $cenyProduktow));
+                $maxOccurrences = max($dominanta);
+                $pierwszaDominanta = array_search($maxOccurrences, $dominanta);
+
+                // Sprawdzenie, czy istnieje więcej niż jedna dominanta
+                foreach ($dominanta as $key => $value) {
+                    if ($value === $maxOccurrences && $key > $pierwszaDominanta) {
+                        $pierwszaDominanta = $key;
                     }
                 }
 
                 // Zaokrąglanie wartości do dwóch miejsc po przecinku
                 $sredniaCenaProduktow = round($sredniaCenaProduktow, 2);
-                $medianaFiltrProduktow = round($medianaFiltrProduktow, 2);
-                $pierwszaDominantaProduktow = round($pierwszaDominantaProduktow, 2);
+                $medianaFiltr = round($medianaFiltr, 2);
+                $pierwszaDominanta = round($pierwszaDominanta, 2);
 
-                // Sprawdzanie, czy istnieją już obliczenia dla danego EAN
-                $checkQuery = "SELECT tk_srednia_cena, tk_mediana, tk_dominanta FROM dbo.tw_konkurencja_obliczenia WHERE tk_plu = ?";
-                $checkParams = array($ean);
-                $checkResult = sqlsrv_query($conn, $checkQuery, $checkParams);
-
-                if ($checkResult === false) {
-                    echo "Błąd podczas sprawdzania istniejących danych dla EAN $ean: " . print_r(sqlsrv_errors(), true);
-                    continue;
-                }
-
-                if (sqlsrv_has_rows($checkResult)) {
-                    $existingRow = sqlsrv_fetch_array($checkResult, SQLSRV_FETCH_ASSOC);
-
-                    // Sprawdzanie, czy wartości są różne
-                    if ($existingRow['tk_srednia_cena'] != $sredniaCenaProduktow || $existingRow['tk_mediana'] != $medianaFiltrProduktow || $existingRow['tk_dominanta'] != $pierwszaDominantaProduktow) {
-                        // Aktualizacja rekordu
-                        $updateQuery = "UPDATE dbo.tw_konkurencja_obliczenia 
-                                        SET tk_ilosc_wystapien = ?, tk_srednia_cena = ?, tk_mediana = ?, tk_dominanta = ?, tk_zaktualizowano = GETDATE() 
-                                        WHERE tk_plu = ?";
-                        $updateParams = array($liczbaProduktowFiltr, $sredniaCenaProduktow, $medianaFiltrProduktow, $pierwszaDominantaProduktow, $ean);
-                        $updateResult = sqlsrv_query($conn, $updateQuery, $updateParams);
-
-                        if ($updateResult === false) {
-                            echo "Błąd podczas aktualizacji danych dla EAN $ean: " . print_r(sqlsrv_errors(), true);
+                                // Sprawdzanie, czy istnieją już obliczenia dla danego EAN
+                                $checkQuery = "SELECT tk_srednia_cena, tk_mediana, tk_dominanta FROM dbo.tw_konkurencja_obliczenia WHERE tk_plu = ?";
+                                $checkParams = array($ean);
+                                $checkResult = sqlsrv_query($conn, $checkQuery, $checkParams);
+                
+                                if ($checkResult === false) {
+                                    echo "Błąd podczas sprawdzania istniejących danych dla EAN $ean: " . print_r(sqlsrv_errors(), true);
+                                    continue;
+                                }
+                
+                                if (sqlsrv_has_rows($checkResult)) {
+                                    $existingRow = sqlsrv_fetch_array($checkResult, SQLSRV_FETCH_ASSOC);
+                
+                                    // Sprawdzanie, czy wartości są różne
+                                    if ($existingRow['tk_srednia_cena'] != $sredniaCenaProduktow || $existingRow['tk_mediana'] != $medianaFiltr || $existingRow['tk_dominanta'] != $pierwszaDominanta) {
+                                        // Aktualizacja rekordu
+                                        $updateQuery = "UPDATE dbo.tw_konkurencja_obliczenia 
+                                                        SET tk_ilosc_wystapien = ?, tk_srednia_cena = ?, tk_mediana = ?, tk_dominanta = ?, tk_zaktualizowano = GETDATE() 
+                                                        WHERE tk_plu = ?";
+                                        $updateParams = array($liczbaProduktowFiltr, $sredniaCenaProduktow, $medianaFiltr, $pierwszaDominanta, $ean);
+                                        $updateResult = sqlsrv_query($conn, $updateQuery, $updateParams);
+                
+                                        if ($updateResult === false) {
+                                            echo "Błąd podczas aktualizacji danych dla EAN $ean: " . print_r(sqlsrv_errors(), true);
+                                        }
+                                    }
+                                } else {
+                                    // Wstawianie nowych wyników do bazy danych
+                                    $insertQuery = "INSERT INTO dbo.tw_konkurencja_obliczenia (
+                                                    tk_plu, tk_ilosc_wystapien, tk_srednia_cena, tk_mediana, tk_dominanta, tk_zaktualizowano
+                                                  ) VALUES (
+                                                    ?, ?, ?, ?, ?, GETDATE()
+                                                  )";
+                                    $insertParams = array($ean, $liczbaProduktowFiltr, $sredniaCenaProduktow, $medianaFiltr, $pierwszaDominanta);
+                                    $insertResult = sqlsrv_query($conn, $insertQuery, $insertParams);
+                
+                                    if ($insertResult === false) {
+                                        echo "Błąd podczas dodawania danych dla EAN $ean: " . print_r(sqlsrv_errors(), true);
+                                    }
+                                }
+                            }
+                        } else {
+                            echo "Brak rekordów do przetworzenia.";
                         }
                     }
-                } else {
-                    // Wstawianie nowych wyników do bazy danych
-                    $insertQuery = "INSERT INTO dbo.tw_konkurencja_obliczenia (
-                                    tk_plu, tk_ilosc_wystapien, tk_srednia_cena, tk_mediana, tk_dominanta, tk_zaktualizowano
-                                  ) VALUES (
-                                    ?, ?, ?, ?, ?, GETDATE()
-                                  )";
-                    $insertParams = array($ean, $liczbaProduktowFiltr, $sredniaCenaProduktow, $medianaFiltrProduktow, $pierwszaDominantaProduktow);
-                    $insertResult = sqlsrv_query($conn, $insertQuery, $insertParams);
-
-                    if ($insertResult === false) {
-                        echo "Błąd podczas dodawania danych dla EAN $ean: " . print_r(sqlsrv_errors(), true);
-                    }
-                }
-            }
-            echo "Dane zostały pomyślnie dodane do bazy danych.";
-        } else {
-            echo "Brak unikalnych numerów EAN w bazie danych.";
-        }
-
-        sqlsrv_free_stmt($wynik_eany);
-        sqlsrv_close($conn);
-    }
-    ?>
+                    ?>
 </body>
 </html>
+                
