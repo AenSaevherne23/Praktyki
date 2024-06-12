6c75fe26964d7e2d5cf1439ff5365374d8e867a0
AenSaevherne23 <kacper.cisek12@gmail.com>
2024-06-12 11:37:21 +0200

    Porownywanie

--- porownaj.php
+++ porownaj.php
@@ -1,34 +1,57 @@
 <?php
 require_once("config.php");
 
-// Zapytanie SQL
+// Parametry paginacji
+$pageNumber = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$rowsPerPage = 100;
+$offset = ($pageNumber - 1) * $rowsPerPage;
+
+// Zapytanie SQL z paginacją
 $sql = "
-SELECT 
-    t.tw_id,
-    t.tw_GT,
-    p.plu_kod,
-    t.tw_nazwa,
-    t.tw_VAT,
-    t.tw_cena_sprz,
-    t.tw_c_zak,
-    t.tw_war_zak,
-    t.tw_pamp,
-    ppmi.tk_ppmi,
-    ppmo.tk_ppmo,
-    ko.tk_srednia_cena AS srednia_cena_konkurencja
-FROM 
-    [leclerc].[dbo].[tw__towar] AS t
-LEFT JOIN 
-    [leclerc].[dbo].[tw_plu] AS p ON p.plu_twid = t.tw_id
-LEFT JOIN 
-    [leclerc].[dbo].[tw_towar_ppmi] AS ppmi ON ppmi.tk_plu = p.plu_kod
-LEFT JOIN 
-    [leclerc].[dbo].[tw_konkurencja_obliczenia] AS ko ON ko.tk_plu = p.plu_kod
-LEFT JOIN 
-    [leclerc].[dbo].[tw_towar_ppmo] AS ppmo ON ppmo.tk_plu = p.plu_kod
-WHERE 
-    ppmi.tk_ppmi IS NOT NULL
-    OR ppmo.tk_ppmo IS NOT NULL;
+DECLARE @PageNumber AS INT = $pageNumber;
+DECLARE @RowsPerPage AS INT = $rowsPerPage;
+
+WITH PagedResults AS (
+    SELECT 
+        t.tw_id,
+        t.tw_GT,
+        LEFT(t.tw_GT, 3) AS gt_nr_3_cyfry,
+        COALESCE(gt.gt_marza, 0) AS gt_marza,
+        p.plu_kod,
+        t.tw_nazwa,
+        t.tw_VAT,
+        t.tw_cena_sprz,
+        t.tw_c_zak,
+        t.tw_war_zak,
+        t.tw_pamp,
+        ppmi.tk_ppmi,
+        ppmo.tk_ppmo,
+        ko.tk_srednia_cena AS srednia_cena_konkurencja,
+        ROW_NUMBER() OVER (ORDER BY t.tw_id) AS RowNum
+    FROM 
+        [leclerc].[dbo].[tw__towar] AS t
+    LEFT JOIN 
+        [leclerc].[dbo].[tw_plu] AS p ON p.plu_twid = t.tw_id
+    LEFT JOIN 
+        [leclerc].[dbo].[tw_towar_ppmi] AS ppmi ON ppmi.tk_plu = p.plu_kod
+    LEFT JOIN 
+        [leclerc].[dbo].[tw_konkurencja_obliczenia] AS ko ON ko.tk_plu = p.plu_kod
+    LEFT JOIN 
+        [leclerc].[dbo].[tw_towar_ppmo] AS ppmo ON ppmo.tk_plu = p.plu_kod
+    LEFT JOIN 
+        (
+            SELECT 
+                LEFT(gt_nr, 3) AS gt_nr_3_cyfry,
+                gt_marza
+            FROM 
+                [leclerc].[dbo].[sl_grupa_tw]
+            WHERE 
+                gt_poziom = 2
+        ) AS gt ON gt.gt_nr_3_cyfry = LEFT(t.tw_GT, 3)
+)
+SELECT * 
+FROM PagedResults
+WHERE RowNum BETWEEN (@PageNumber - 1) * @RowsPerPage + 1 AND @PageNumber * @RowsPerPage;
 ";
 
 // Wykonanie zapytania
@@ -43,6 +66,7 @@ echo "<table border='1'>";
 echo "<tr>
         <th>ID</th>
         <th>GT</th>
+        <th>GT Marża</th>
         <th>PLU</th>
         <th>Nazwa</th>
         <th>VAT</th>
@@ -59,6 +83,7 @@ while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
     echo "<tr>";
     echo "<td>" . htmlspecialchars($row['tw_id']) . "</td>";
     echo "<td>" . htmlspecialchars($row['tw_GT']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['gt_marza']) . "</td>";
     echo "<td>" . htmlspecialchars($row['plu_kod']) . "</td>";
     echo "<td>" . htmlspecialchars($row['tw_nazwa']) . "</td>";
     echo "<td>" . htmlspecialchars($row['tw_VAT']) . "</td>";
@@ -73,6 +98,14 @@ while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
 }
 
 echo "</table>";
+
+// Wyświetlenie linków paginacji
+echo "<div>";
+if ($pageNumber > 1) {
+    echo "<a href=\"?page=" . ($pageNumber - 1) . "\">Poprzednia</a> ";
+}
+echo "<a href=\"?page=" . ($pageNumber + 1) . "\">Następna</a>";
+echo "</div>";
 
 // Zamknięcie połączenia
 sqlsrv_free_stmt($stmt);
